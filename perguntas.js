493f721a503ea6896df7612ff0fe6a1e057f0aff
simon.esquilo <00001108984125sp@al.educacao.sp.gov.br>
2024-10-29 14:58:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Inglês',
+    'Como se escreve "gato" em inglês?',
+    'Gato em inglês é CAT'
+)
+
+criaCartao(
+    'Inglês',
+    'How do we write "strawberry" in Portuguese?',
+    'Strawberry in Portuguese is MORANGO'
+)
+
+criaCartao(
+    'Inglês',
+    'Qual país chamamos de Switzerland?',
+    'Chamamos a SUÍÇA de Switzerland'
+)
+
+criaCartao(
+    'Inglês',
+    'How do we call a mouse in Portuguese?',
+    'We call it RATO'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz feliz aniversário em Inglês?',
+    'Se diz HAPPY BIRTHDAY'
+)
+
+criaCartao(
+    'Inglês',
+    'Como dizemos "de nada" em Inglês?',
+    'Dizemos YOURE WELCOME'
+)
